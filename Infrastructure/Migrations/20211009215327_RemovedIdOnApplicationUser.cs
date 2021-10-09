316938fa939c143a68bb930b98047386acfb6280
KylerHansen <kylerhansen@mail.weber.edu>
2021-10-09 16:07:49 -0600

    Update users is now working

---
+++ Infrastructure/Migrations/20211009215327_RemovedIdOnApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class RemovedIdOnApplicationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
